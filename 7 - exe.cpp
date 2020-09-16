67b921a614ffa98f85a5dd1a75ca3235320959d1
unknown <j.c.rampim@gmail.com>
2020-09-16 17:18:20 -0300

    Atualização de exercício

---
+++ 7 - exe.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+for(int i = 0; i <= 500; i++){
+		cout << "Os números multiplos de  são: "<<i << " = " <<(i * 5) << endl;
+
+}
+        return 0;
+}
